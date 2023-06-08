6d162a7fa7cb888846e9e3710e0b6199250ccb58
Peter Eldritch <peter.jirak@gmail.com>
2023-06-08 14:56:39 -0700

    The purpose of this project is to follow along with Nick Scialli's DevTuts tutorial "An Introduction to Test-Driven Development in JavaScript". This is my first code commit, I used npm init to initialize the project. I now have a package-lock.json file thagt contains my dependencies, a package.json file that deswcribes my project/packagge and indicates I am using jest for automated testing. I have a adder.js function that will provide my adder function (the focus of this tutorial, it presently exports no code. I also have adder.test.js that provides a test of the adder function. That test presently fails because the adder function does not yet exist. This is how test driven development works, one writes the test first, it fails, and then one writes sufficient code for the test to pass and one refactors as necessary. This is also known as "red-green refactor".

---
+++ adder.test.js
@@ -0,0 +1,12 @@
+const adder = require("./adder");
+
+describe(
+    "adder",
+    () => {
+        it("should add two numbers",
+            () => {
+                expect(adder(1, 2).toBe(3))
+            }
+        )
+    }
+)
