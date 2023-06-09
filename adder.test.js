867a2fa9d0a7bfb4febf245b13d0f1f0c1fb3323
Peter Eldritch <peter.jirak@gmail.com>
2023-06-08 19:56:31 -0700

    Fixed syntax error - parentheses were placed incorrectly. I fixed them.

--- adder.test.js
+++ adder.test.js
@@ -5,7 +5,7 @@ describe(
     () => {
         it("should add two numbers",
             () => {
-                expect(adder(1, 2).toBe(3))
+                expect(adder(1, 2)).toBe(3)
             }
         )
     }
