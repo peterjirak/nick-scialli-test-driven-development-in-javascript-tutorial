c660816fd80ec4378bcefc8ede6686d8dddafa8a
Peter Eldritch <peter.jirak@gmail.com>
2023-06-09 07:42:56 -0700

    I am doing this exercise to re-familiarize myself with test-driven development (also known as red-green-refactor). This problem is very simple compared to real life computer programming. That said, in step 1 we wrote a simple test for adder -- add two numbers; it failed because the functionality did not exist (the program was empty); in step two we wrote enough code to get the test to pass; in step three we switched from a conventional function defined using the keyword function to an arrow function. In this step we thought about adder. We had originally written adder to add two numbers, but in reality such a function should likely work on one or more numbers. So, originally we had one failing test that added two numbers. We implemented adder and it passed that test. We changed adder to be an arrow function and it still passed the test. Now we add two more tests, invoke adder with just one number and invoke adder with three numbers. The original test still passed, but the two new one's fall. In the next xommit we will make it so that the two new tests pass.

--- adder.test.js
+++ adder.test.js
@@ -7,6 +7,16 @@ describe(
             () => {
                 expect(adder(1, 2)).toBe(3)
             }
+        );
+        it("should be able to handle a sole input",
+            () => {
+                expect(adder(5)).toBe(5)
+            }
+        );
+        it("should add three numbers",
+            () => {
+                expect(adder(1, 2, 3)).toBe(6)
+            }
         )
     }
 )
